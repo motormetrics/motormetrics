b91a77f29c39bdb01227803b655d171c710ab36b
Ru Chern CHONG <ruchern.chong@motormetrics.app>
2026-05-15 00:59:35 +0800

    feat(web): set up next-intl

--- apps/web/src/app/providers.tsx
+++ apps/web/src/app/providers.tsx
@@ -1,13 +1,22 @@
 "use client";
 
 import { Toast } from "@heroui/react";
+import { type AbstractIntlMessages, NextIntlClientProvider } from "next-intl";
 import type { ReactNode } from "react";
 
-export function Providers({ children }: Readonly<{ children: ReactNode }>) {
+export function Providers({
+  children,
+  locale,
+  messages,
+}: Readonly<{
+  children: ReactNode;
+  locale: string;
+  messages: AbstractIntlMessages;
+}>) {
   return (
-    <>
+    <NextIntlClientProvider locale={locale} messages={messages}>
       <Toast.Provider placement="bottom end" />
       {children}
-    </>
+    </NextIntlClientProvider>
   );
 }
